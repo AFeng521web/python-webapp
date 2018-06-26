e675b9db4e6d352ea4aca60ebef3e5292d3f30df
AFeng521web <AFeng521web@163.com>
2018-06-26 20:33:37 +0800

    handle exceptional

--- DBcm.py
+++ DBcm.py
@@ -1,60 +1,35 @@
-"""The UseDatabase context manager for working with MySQL/MariaDB.
-
-For more information, see Chapters 7, 8, 9, and 11 of the 2nd edition of
-Head First Python.
-
-Simple example usage:
-
-    from DBcm import UseDatabase, SQLError
-
-    config = { 'host': '127.0.0.1',
-               'user': 'myUserid',
-               'password': 'myPassword',
-               'database': 'myDB' }
-
-    with UseDatabase(config) as cursor:
-        _SQL = "select * from log"
-        cursor.execute(_SQL)
-        data = cursor.fetchall()
-
-Enjoy, and have fun.  (Sorry: Python 3 only, due to type hints and new syntax).
-"""
-
-##############################################################################
-# Context manager for connecting/disconnecting to a database.
-##############################################################################
-
 import mysql.connector
 
+class ConnectionError(Exception):
+    pass
 
-class UseDatabase:
-    def __init__(self, config: dict):
-        """Add the database configuration parameters to the object.
+class CredentialsError(Exception):
+    pass
 
-        This class expects a single dictionary argument which needs to assign
-        the appropriate values to (at least) the following keys:
+class SQLError(Exception):
+    pass
 
-            host - the IP address of the host running MySQL/MariaDB.
-            user - the MySQL/MariaDB username to use.
-            password - the user's password.
-            database - the name of the database to use.
+class UseDatabase(object):
+          def __init__(self, config: dict):
+              self.configuration = config
 
-        For more options, refer to the mysql-connector-python documentation.
-        """
-        self.configuration = config
+          def __enter__(self) ->"cursor":
+              try:
+                  self.conn = mysql.connector.connect(**self.configuration)
+                  self.cursor = self.conn.cursor()
+                  return self.cursor
+              except mysql.connector.errors.InterfaceError as err:
+                  raise ConnectionError(err)
+              except mysql.connector.errors.ProgrammingError as err:
+                  raise CredentialsError(err)
 
-    def __enter__(self) -> 'cursor':
-        """Connect to database and create a DB cursor.
 
-        Return the database cursor to the context manager.
-        """
-        self.conn = mysql.connector.connect(**self.configuration)
-        self.cursor = self.conn.cursor()
-        return self.cursor
-
-    def __exit__(self, exc_type, exc_value, exc_traceback):
-        """Destroy the cursor as well as the connection (after committing).
-        """
-        self.conn.commit()
-        self.cursor.close()
-        self.conn.close()
+          def __exit__(self, exc_type, exc_value, exc_traceback):
+                    self.conn.commit()
+                    self.cursor.close()
+                    self.conn.close()
+                    if exc_type is mysql.connector.errors.ProgrammingError:
+                        raise SQLError(exc_value)
+                    elif exc_type:
+                        raise exc_type(exc_value)
+                    
